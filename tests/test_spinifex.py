d6c67d1ae5112f66254c178bcdbf9a064ecf9836
Tammo Jan Dijkema <dijkema@astron.nl>
2024-11-12 11:46:06 +0100

    Add test

---
+++ tests/test_spinifex.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from spinifex import get_bla
+
+
+def test_get_bla():
+    assert get_bla(4.0) == 12.0
+    assert get_bla(5.0) == 15.0
+    assert get_bla(-9.0) == -27.0
+    assert get_bla(3) == 9
