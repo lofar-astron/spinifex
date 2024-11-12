3e16fa0afba7992676fcd94e99b108904ec8cca9
maaijke <mevius@astron.nl>
2024-11-12 15:52:22 +0100

    add numpy

--- tests/test_spinifex.py
+++ tests/test_spinifex.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
-from spinifex import get_bla
-
+from spinifex import get_bla, get_blo
+import numpy as np
 
 def test_get_bla():
     assert get_bla(4.0) == 12.0
     assert get_bla(5.0) == 15.0
     assert get_bla(-9.0) == -27.0
     assert get_bla(3) == 9
+
+def test_get_blo():
+    assert get_blo(4.0) == 12.0
+    assert get_blo(np.arange(5)) == np.arange(5)*3
+
