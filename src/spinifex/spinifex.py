a029d3744cadb19118ed0dbdeb9e05df168cc7ea
maaijke <mevius@astron.nl>
2024-11-12 16:29:11 +0100

    pre-commit change

--- src/spinifex/spinifex.py
+++ src/spinifex/spinifex.py
@@ -1,13 +1,15 @@
 """All the stuff that is not in submodules"""
 
 from __future__ import annotations
+
 import numpy as np
+
 
 def get_bla(my_input: float) -> float:
     """Compute three times input"""
     return my_input * 3
 
+
 def get_blo(my_input: np.ndarray) -> np.ndarray:
     """Compute three times input"""
     return my_input * 3
-
--- tests/test_spinifex.py
+++ tests/test_spinifex.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from spinifex import get_bla, get_blo
 import numpy as np
+
+from spinifex import get_bla, get_blo
+
 
 def test_get_bla():
     assert get_bla(4.0) == 12.0
@@ -9,7 +11,7 @@ def test_get_bla():
     assert get_bla(-9.0) == -27.0
     assert get_bla(3) == 9
 
+
 def test_get_blo():
     assert get_blo(4.0) == 12.0
-    assert get_blo(np.arange(5)) == np.arange(5)*3
-
+    assert get_blo(np.arange(5)) == np.arange(5) * 3
