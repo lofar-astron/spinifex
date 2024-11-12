922612a78414eed5cf17729918663a7c3d8f8aa9
Tammo Jan Dijkema <dijkema@astron.nl>
2024-11-12 11:59:05 +0100

    Fix test

--- tests/test_spinifex.py
+++ tests/test_spinifex.py
@@ -6,5 +6,5 @@ from spinifex import get_bla
 def test_get_bla():
     assert get_bla(4.0) == 12.0
     assert get_bla(5.0) == 15.0
-    assert get_bla(-9.0) == -26.0
+    assert get_bla(-9.0) == -27.0
     assert get_bla(3) == 9
