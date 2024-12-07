0e1d437d13f960fe983b5c77606cd2f24577be25
maaijke <mevius@astron.nl>
2024-12-07 23:38:39 +0100

    simple ipp model

--- spinifex/geometry/get_ipp.py
+++ spinifex/geometry/get_ipp.py
@@ -12,6 +12,8 @@ import numpy as np
 from astropy.coordinates import ITRS, AltAz, EarthLocation, SkyCoord
 from astropy.time import Time
 
+r_earth = 6364.62 * u.km
+
 
 class IPP(NamedTuple):
     loc: EarthLocation
@@ -23,23 +25,84 @@ class IPP(NamedTuple):
 
 
 def get_ipp_from_skycoord(
-    loc: EarthLocation, times: Time, source: SkyCoord, height_array: np.ndarray[float]
+    loc: EarthLocation, times: Time, source: SkyCoord, height_array: u.Quantity
 ) -> IPP:
-    """Get the ionospheric piercepoints as EarhtLocations for a given EarthLocation, time, SkyCoord"""
-    # height_array = np.arange(100, 1000, 10) * u.km
+    """Get ionospheric piercepoints
+
+    Parameters
+    ----------
+    loc : EarthLocation
+        observer location
+    times : Time
+        observation times
+    source : SkyCoord
+        source location
+    height_array : u.Quantity
+        array of altitudes
+
+    Returns
+    -------
+    IPP
+        Ionospheric piercepoints
+    """
     aa = AltAz(location=loc, obstime=times)
     altaz = source.transform_to(aa)
-    return get_ipp_from_altaz(loc, altaz, height_array, times)
+    return get_ipp_from_altaz(loc, altaz, height_array)
 
 
 def get_ipp_from_altaz(
-    loc: EarthLocation, altaz: AltAz, height_array: np.ndarray[float], times: Time
+    loc: EarthLocation, altaz: AltAz, height_array: u.Quantity
 ) -> IPP:
-    altaz_dir = altaz.transform_to(ITRS)
-    ipp_vector = altaz_dir.cartesian[np.newaxis] * height_array[:, np.newaxis]
-    ipp = [
-        loc.to(u.m).x + ipp_vector.x,
-        loc.to(u.m).y + ipp_vector.y,
-        loc.to(u.m).z + ipp_vector.z,
+    """get ionospheric piercepoints from azimuth elevations
+
+    Parameters
+    ----------
+    loc : EarthLocation
+        observer location
+    altaz : AltAz
+        azimuth and elevations for all times
+    height_array : u.Quantity
+        array of altitudes
+
+    Returns
+    -------
+    IPP
+        _description_
+    """
+    los_dir = altaz.transform_to(ITRS)
+    ipp = _get_ipp_simple(height_array=height_array, loc=loc, los_dir=los_dir)
+    return IPP(
+        loc=EarthLocation.from_geocentric(*ipp), times=altaz.obstime, los=los_dir
+    )
+
+
+def _get_ipp_simple(
+    height_array: u.Quantity, loc: EarthLocation, los_dir: SkyCoord
+) -> list[u.Quantity]:
+    """helper function to calculate ionospheric piercepoints using a simple spherical earth model
+
+    Parameters
+    ----------
+    height_array : u.Quantity
+        array of altitudes
+    loc : EarthLocation
+        observer location
+    los_dir : SkyCoord
+        line of sight (ITRS)
+
+    Returns
+    -------
+    list[u.Quantity]
+        ipp.x, ipp.y, ipp.z positions
+    """
+    c_value = r_earth**2 - (r_earth + height_array) ** 2
+    ipp_vector = los_dir.cartesian[np.newaxis] * height_array[:, np.newaxis]
+    b_value = np.sqrt(
+        loc.x * ipp_vector.x + loc.y * ipp_vector.y + loc.z * ipp_vector.z
+    )  # inproduct
+    alphas = -b_value + np.sqrt(b_value**2 - c_value[:, np.newaxis])
+    return [
+        loc.x + alphas * los_dir.x,
+        loc.y + alphas * los_dir.y,
+        loc.z + alphas * los_dir.z,
     ]
-    return IPP(loc=EarthLocation.from_geocentric(*ipp), times=times, los=altaz_dir)
