9d8d1d7ae5c7cb02e443131671b5e29a3a0694f3
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2025-01-22 17:52:02 +1100

    Docs

--- spinifex/asyncio.py
+++ spinifex/asyncio.py
@@ -20,16 +20,22 @@ T = TypeVar("T")
 
 
 def sync_wrapper(coro: Callable[P, Coroutine[None, None, T]]) -> Callable[P, T]:
+    # Apply the `nest_asyncio` magic to allow nested event loops
+    # Need for running async functions in a Jupyter notebook
     nest_asyncio.apply()
 
     @wraps(coro)
     def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         try:
+            # Check if we are in an event loop
             loop = asyncio.get_running_loop()
         except RuntimeError:
-            # No running loop: safe to use asyncio.run
+            # Nope, we are not in an event loop
+            # Create a new event loop with `asyncio.run`
             return asyncio.run(coro(*args, **kwargs))
-        else:
-            return loop.run_until_complete(coro(*args, **kwargs))
+        # We are in an event loop
+        # Use the current event loop to run the coroutine
+        # This is useful when we are in a Jupyter notebook
+        return loop.run_until_complete(coro(*args, **kwargs))
 
     return wrapper
