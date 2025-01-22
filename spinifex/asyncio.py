821e587ef619616c02b69c206780e5b69ac5d6a8
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2025-01-22 17:36:24 +1100

    Add nest

--- spinifex/asyncio.py
+++ spinifex/asyncio.py
@@ -8,6 +8,8 @@ from collections.abc import Coroutine
 from functools import wraps
 from typing import Callable, TypeVar
 
+import nest_asyncio
+
 if sys.version_info < (3, 10):
     from typing_extensions import ParamSpec
 else:
@@ -18,22 +20,16 @@ T = TypeVar("T")
 
 
 def sync_wrapper(coro: Callable[P, Coroutine[None, None, T]]) -> Callable[P, T]:
+    nest_asyncio.apply()
+
     @wraps(coro)
-    def wrapper(
-        *args: P.args,
-        **kwargs: P.kwargs,
-    ) -> T:
-        # First see if we are already in an event loop
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
         try:
             loop = asyncio.get_running_loop()
         except RuntimeError:
-            # Nope - we can go ahead and create one
+            # No running loop: safe to use asyncio.run
             return asyncio.run(coro(*args, **kwargs))
+        else:
+            return loop.run_until_complete(coro(*args, **kwargs))
 
-        # We are in an event loop, so we can't use run
-        # Submit the coroutine to the event loop
-        return asyncio.run_coroutine_threadsafe(coro(*args, **kwargs), loop).result()
-
-    # Keep the function docs correct
-    wrapper.__doc__ = coro.__doc__
     return wrapper
